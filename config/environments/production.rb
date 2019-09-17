dbe88b9b4cfc8fad3db24c5e19ab8666f6ad3922
absarora <absingharora@gmail.com>
2019-09-18 00:53:40 +0200

    update the heroku app name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,5 +91,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { :host => 'volunteer-management.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'vm-backend.herokuapp.com' }
 end
